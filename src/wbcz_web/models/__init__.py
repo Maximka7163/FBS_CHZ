cb58aa987192b5331d6748e4097c9c1bb66df3d1
Maximka7163 <maxim7163@yandex.ru>
2026-09-18 01:06:32 +0500

    feat: export M8 SUZ models

--- src/wbcz_web/models/__init__.py
+++ src/wbcz_web/models/__init__.py
@@ -1,11 +1,14 @@
 from .db import AuditLog, Base, CheckRecord, ControlRun, EventRecord, ImportRecord, ImportRow, PreviewItem, PreviewRecord, SessionRecord, User
 from .agent import AgentJobRecord, DocumentLifecycleLedgerRecord, TurnoverOperationLedgerRecord, WriteAuditRecord, WriteOperationRecord
 from .edo_lite import EdoLiteAnnualQuotaRecord, EdoLiteLedgerRecord, EdoLiteSchemaRegistryRecord
+from .suz import SuzCodeBlockRecord, SuzConnectionRecord, SuzKmVaultRecord, SuzOrderItemRecord, SuzOrderRecord, SuzReconciliationEventRecord
 
 __all__ = [
     "AuditLog", "Base", "CheckRecord", "ControlRun", "EventRecord", "ImportRecord",
     "ImportRow", "PreviewItem", "PreviewRecord", "SessionRecord", "User",
     "AgentJobRecord", "DocumentLifecycleLedgerRecord", "TurnoverOperationLedgerRecord",
     "WriteAuditRecord", "WriteOperationRecord", "EdoLiteLedgerRecord",
-    "EdoLiteSchemaRegistryRecord", "EdoLiteAnnualQuotaRecord",
+    "EdoLiteSchemaRegistryRecord", "EdoLiteAnnualQuotaRecord", "SuzConnectionRecord",
+    "SuzOrderRecord", "SuzOrderItemRecord", "SuzCodeBlockRecord", "SuzKmVaultRecord",
+    "SuzReconciliationEventRecord",
 ]
