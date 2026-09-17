62c06bfbd950db310c85ff18fa12ddb856e81dd2
Maximka7163 <maxim7163@yandex.ru>
2026-09-17 10:41:25 +0500

    feat: export M4 lifecycle ledger model

--- src/wbcz_web/models/__init__.py
+++ src/wbcz_web/models/__init__.py
@@ -1,8 +1,8 @@
 from .db import AuditLog, Base, CheckRecord, ControlRun, EventRecord, ImportRecord, ImportRow, PreviewItem, PreviewRecord, SessionRecord, User
-from .agent import AgentJobRecord, WriteAuditRecord, WriteOperationRecord
+from .agent import AgentJobRecord, DocumentLifecycleLedgerRecord, WriteAuditRecord, WriteOperationRecord
 
 __all__ = [
     "AuditLog", "Base", "CheckRecord", "ControlRun", "EventRecord", "ImportRecord",
     "ImportRow", "PreviewItem", "PreviewRecord", "SessionRecord", "User",
-    "AgentJobRecord", "WriteAuditRecord", "WriteOperationRecord",
+    "AgentJobRecord", "DocumentLifecycleLedgerRecord", "WriteAuditRecord", "WriteOperationRecord",
 ]
