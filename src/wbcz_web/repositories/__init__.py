ba276abb92321daca0ec90ed099536baaccfa3b6
Maximka7163 <maxim7163@yandex.ru>
2026-09-18 06:36:03 +0500

    feat(m11): export report repositories

--- src/wbcz_web/repositories/__init__.py
+++ src/wbcz_web/repositories/__init__.py
@@ -3,8 +3,10 @@ from .imports import ImportRepository
 from .sessions import SessionRepository
 from .users import UserRepository
 from .agent import AgentJobMetadata, SqlAlchemyAgentJobStore, SqlAlchemyWriteOperationStore
+from .reports import ClaimedReportJob, SqlReportRepository, SqlUploadBindingStore
 
 __all__ = [
     "AuditRepository", "ImportRepository", "SessionRepository", "UserRepository",
     "AgentJobMetadata", "SqlAlchemyAgentJobStore", "SqlAlchemyWriteOperationStore",
+    "ClaimedReportJob", "SqlReportRepository", "SqlUploadBindingStore",
 ]
