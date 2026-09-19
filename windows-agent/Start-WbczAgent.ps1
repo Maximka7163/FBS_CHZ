e065725a4f5d40ac9215ab2973ca9a01485fbb2a
Maximka7163 <maxim7163@yandex.ru>
2026-09-19 13:46:33 +0500

    feat(rc1): add outbound Windows agent launcher

---
+++ windows-agent/Start-WbczAgent.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = 'Stop'
+. (Join-Path $PSScriptRoot 'Runtime-Common.ps1')
+
+$config = Get-WbczConfig
+Set-WbczRuntimeEnvironment -Config $config
+$exe = Get-WbczAgentExe
+
+Write-Output 'AGENT_MODE=OUTBOUND_ONLY'
+Write-Output 'PRODUCTION_WRITE=false'
+& $exe run
+exit $LASTEXITCODE
